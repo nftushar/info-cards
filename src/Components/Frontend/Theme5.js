420d7cb10be5435c0f260e3ca2f4aab0bf1484b9
nftushar <nftushar5200@gmail.com>
2023-07-15 10:18:56 +0600

    WOrking On frontend Part

--- src/Components/Frontend/Theme5.js
+++ src/Components/Frontend/Theme5.js
@@ -1,6 +1,25 @@
 const Theme5 = ({ attributes, card, index }) => {
-    const { theme } = attributes;
+    const { theme, isImg, imgPos } = attributes;
 
-    return <div className={`card card-${index} ${theme}`} key={index}>new card</div>
+    const { img, title, desc, btnLabal, btnUrl } = card;
+    const imgEl = isImg && img && <img src={img} alt={title} />
+
+    return <div className="theme5-cards"> <div className={`theme5 card-${index} ${theme}`} key={index} >
+        <div className="imgBox">
+            {'first' === imgPos && imgEl}
+        </div>
+        <div className="content">
+            <div className="details">
+                <h2>{title}</h2>
+                <p>{desc}</p>
+                <div className="actionBtn">
+                    <button href={btnUrl} >{btnLabal}</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+        {'last' === imgPos && imgEl}
+    </div>
 }
 export default Theme5;
